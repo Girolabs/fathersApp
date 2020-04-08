30b92a2e8b7bc40f232f4a63a40b092db4707df0
Guillermo <guillermocaba95@gmail.com>
2020-04-08 10:58:06 -0400

    initial commit, eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "no-underscore-dangle":0,
+    "arrow-body-style":0,
+    "no-shadow":0,
+    "consistent-return":0,
+    "no-nested-ternary":0,
+    "no-console":0,
+    "no-case-declarations":0,
+    "import/prefer-default-export":0
+
+  },
+};
