bcb4aac385c81c16709e31c54a73d3736c890dd1
Guillermo <guillermocaba95@gmail.com>
2020-04-16 13:08:17 -0400

    feat: add search by name and lastname & sketch of patreDetail

--- src/screens/PatreDetailScreen.jsx
+++ src/screens/PatreDetailScreen.jsx
@@ -1,16 +1,158 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, SectionList } from "react-native";
+import Colors from '../constants/Colors';
 
-const PatreDetailScreen = () => {
+const PatreDetailScreen = ({ navigation }) => {
+    const profile = navigation.getParam('profile');
+    console.log(profile)
+    const DATA= [
+        {
+            title:'Información del contacto',
+            data: [
+                {
+                    subtitle: 'Email',
+                    body:profile.email,
+                },
+                {
+                    subtitle: 'Teléfono móvil principal',
+                    body:profile.phones[0].number,
+                },
+                {
+                    subtitle: 'Casa',
+                    body:profile.phones[1] != undefined ? profile.phones[1].number: '',
+                },
+            ]
+        }
+    ]
+    const DATA2 = [
+        {
+            title:'Vivienda actual',
+            data: [
+                {
+                    subtitle: 'Filial',
+                    body:'',
+                },
+                {
+                    subtitle: 'Casa',
+                    body:profile.phones[0].number,
+                },
+                {
+                    subtitle: 'Territorio responsable /  Donde trabaja: ',
+                    body:profile.phones[1] != undefined ? profile.phones[1].number: '',
+                },
+            ]
+        } 
+    ]
+
+    const DATA3 = [
+        {
+            title:'Informacion Personal',
+            data: [
+                {
+                    subtitle: 'Pais de origen',
+                    body:''
+                },
+                {
+                    subtitle: 'Territorio de origen',
+                    body:profile.phones[0].number,
+                },
+                {
+                    subtitle: 'Curso',
+                    body:profile.phones[1] != undefined ? profile.phones[1].number: '',
+                },
+
+                {
+                    subtitle: 'Generación',
+                    body:profile.email,
+                },
+                {
+                    subtitle: 'Cumpleaños ',
+                    body:profile.phones[0].number,
+                },
+                {
+                    subtitle: 'Onomástico',
+                    body:profile.phones[1] != undefined ? profile.phones[1].number: '',
+                },
+                {
+                    subtitle: 'Bautismo',
+                    body:profile.phones[1] != undefined ? profile.phones[1].number: '',
+                },
+                {
+                    subtitle: 'Admisión al Postulantado',
+                    body:profile.phones[1] != undefined ? profile.phones[1].number: '',
+                },
+                {
+                    subtitle: 'Inicio del Noviciado',
+                    body:profile.phones[1] != undefined ? profile.phones[1].number: '',
+                },
+                {
+                    subtitle: 'Pertenencia Comunitaria',
+                    body:profile.phones[1] != undefined ? profile.phones[1].number: '',
+                },
+                {
+                    subtitle: 'Contrato Perpetuo',
+                    body:profile.phones[1] != undefined ? profile.phones[1].number: '',
+                },
+                {
+                    subtitle: 'Ordenación Diaconal',
+                    body:profile.phones[1] != undefined ? profile.phones[1].number: '',
+                },
+                {
+                    subtitle: 'Ordenación Sacerdotal',
+                    body:profile.phones[1] != undefined ? profile.phones[1].number: '',
+                },
+            ]
+        } 
+    ]
+
     return (
-        <View>
-            <Text>PatreDetail</Text>
+        <View style={styles.screen}>
+            <View style={{ flexDirection: 'row',alignItems:'center'}}>
+                <Image style={{ width:100, height: 100, borderRadius:50  }} resizMode='center' source={{uri:`https://schoenstatt-fathers.link${profile.photo}`}} />
+                <View style={{ padding:15}}>
+                    <Text style={{fontFamily:'work-sans-semibold',fontSize:18,color:Colors.onSurfaceColorPrimary}}>{profile.fullName}</Text>
+                    <Text style={{ color: Colors.onSurfaceColorSecondary, fontFamily: 'work-sans'}}>{`Last update:${profile.personalInfoUpdatedOn}`}</Text>
+                </View>
+            </View>
+            <SectionList
+                sections = {DATA}
+                renderItem={({item})=>
+                <View 
+                    style={{ paddingVertical:15, backgroundColor: Colors.surfaceColorSecondary}}>
+                    <Text >{item.subtitle}</Text>
+                    <Text>{item.body}</Text>
+                </View>
+                    }
+                renderSectionHeader={({section:{title}})=> (
+                    <Text style={{ fontFamily:'work-sans',color:Colors.onSurfaceColorPrimary, fontSize:11}}>{title}</Text>
+                )}
+            
+            />
+
+             <SectionList
+                sections = {DATA2}
+                renderItem={({item})=>
+                <View 
+                    style={{ paddingVertical:15, backgroundColor: Colors.surfaceColorSecondary}}>
+                    <Text >{item.subtitle}</Text>
+                    <Text>{item.body}</Text>
+                </View>
+                    }
+                renderSectionHeader={({section:{title}})=> (
+                    <Text style={{ fontFamily:'work-sans',color:Colors.onSurfaceColorPrimary, fontSize:11}}>{title}</Text>
+                )}
+            
+            />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    screen: {},
+    screen: {
+        flex:1,
+        padding:15,
+        backgroundColor: Colors.surfaceColorPrimary
+    },
 });
 
 export default PatreDetailScreen;
--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,20 +1,103 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TextInput, FlatList, TouchableOpacity } from "react-native";
+import Colors from '../constants/Colors';
+import { Ionicons } from "expo-vector-icons";
+import Constants from 'expo-constants';
+import axios from 'axios';
 
-const SearchScreen = () => {
-    return (
-        <View style={styles.screen}>
-            <Text>SearchScreen</Text>
-        </View>
-    );
+class SearchScreen extends Component{
+    state = {
+        results: [],
+        filterResults: [],
+    }
+
+    componentDidMount () {
+        axios.
+            get(`https://schoenstatt-fathers.link/en/api/v1/persons?key=${Constants.manifest.extra.secretKey}`)
+            .then((res) => {
+                if(res.status == 200) {
+                    this.setState({ results:res.data.result})
+                }
+            })
+    }
+    handleFilter =(keyword) => {
+        console.log(keyword)
+        let filterResults = [];
+        const texto = keyword.toLowerCase();
+        filterResults = this.state.results.filter((persona) => {
+            if ((persona.firstNameWithoutAccents + ' ' + persona.lastNameWithoutAccents).trim().startsWith(texto)) {
+                return persona;
+            }
+            if (persona.firstNameWithoutAccents.trim().startsWith(texto)) {
+                return persona;
+            }
+            if (persona.lastNameWithoutAccents.trim().startsWith(texto)) {
+                return persona;
+            }
+        });
+        this.setState({ filterResults: filterResults });
+    }
+    render(){
+        return (
+            <View style={styles.screen}>
+                <View style={styles.inputBox}>
+                    <TextInput placeholder="Search" onChangeText={(text) => this.handleFilter(text)} />
+                    <Ionicons name='ios-search' size={25} colors={Colors.primaryColor} />
+                </View>
+                <FlatList
+                    data={this.state.filterResults}
+                    renderItem ={({item,data})=> {
+                        return(
+                            <TouchableOpacity
+                                style={styles.item}
+                                onPress={() => {
+                                    this.props.navigation.navigate('PatreDetail',{
+                                        profile: item
+                                    })
+                                }}
+                                >
+                                <Text>{item.fullName}</Text>
+                                <Ionicons name="ios-arrow-forward" size={23} color={Colors.primaryColor} />
+                            </TouchableOpacity>
+                        )
+                    }}
+                
+                />
+              
+            </View>
+        );
+    }
+   
 };
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+      /*   justifyContent: "center",
+        alignItems: "center", */
+        padding:15,
+        backgroundColor:Colors.surfaceColorPrimary
     },
+    inputBox: {
+        backgroundColor:Colors.surfaceColorSecondary,
+        width:'100%',
+        height:50,
+        flexDirection:'row',
+        borderRadius:15,
+        padding:15,
+        justifyContent:'space-between',
+        marginBottom:15
+    },
+    item: {
+        backgroundColor: Colors.surfaceColorSecondary,
+        padding:15,
+        borderBottomColor: Colors.surfaceColorPrimary,
+        borderBottomWidth:1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderRadius:15
+    }
 });
 
 export default SearchScreen;
