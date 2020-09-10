2ef9f26be5a7c041b653981ed5ecf62dbfd2f41d
Joaquin Caballero <joaquinecc@gmail.com>
2020-09-10 11:11:00 -0400

    en ios se utiliza ActionSheet como picker

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Picker, StyleSheet } from 'react-native';
+import { ActionSheetIOS, Text, View, Picker, StyleSheet, Platform, Button } from 'react-native';
 
 import Colors from '../constants/Colors';
+import { result } from 'lodash';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,16 +13,38 @@ const styles = StyleSheet.create({
   },
 });
 
-const Select = ({
-  value, elements, valueChange, style,
-}) => {
+const Select = ({ value, elements, valueChange, style }) => {
+  const [result, setResult] = React.useState(elements[0].name);
+  const [options, setOption] = React.useState([]);
+  React.useEffect(() => {
+    const options = [];
+    elements.forEach((e) => {
+      options.push(e.name);
+      if (value === e.value) setResult(e.name);
+    });
+    setOption(options);
+  }, []);
+  const onPress = () =>
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: options,
+      },
+      (buttonIndex) => {
+        valueChange(elements[buttonIndex].value);
+        setResult(elements[buttonIndex].name);
+      },
+    );
   return (
     <View style={{ ...styles.container, ...style }}>
-      <Picker selectedValue={value} onValueChange={(itemValue) => valueChange(itemValue)}>
-        {elements.map((el) => {
-          return <Picker.Item color={Colors.onSurfaceColorPrimary} label={el.name} value={el.value} />;
-        })}
-      </Picker>
+      {Platform.OS === 'android' ? (
+        <Picker selectedValue={value} onValueChange={(itemValue) => valueChange(itemValue)}>
+          {elements.map((el) => {
+            return <Picker.Item color={Colors.onSurfaceColorPrimary} label={el.name} value={el.value} />;
+          })}
+        </Picker>
+      ) : (
+        <Button color={Colors.onSurfaceColorPrimary} onPress={onPress} title={result} />
+      )}
     </View>
   );
 };
