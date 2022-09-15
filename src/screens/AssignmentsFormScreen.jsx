79b1268107959d3fc2229b371b9ec1efbcf7d8cc
AlexGC <alex1995gc@gmail.com>
2022-09-15 17:40:00 -0400

    persons comboBox

--- src/screens/AssignmentsFormScreen.jsx
+++ src/screens/AssignmentsFormScreen.jsx
@@ -32,6 +32,7 @@ import RNPickerSelect from 'react-native-picker-select';
 import Input from '../components/Input';
 import { TextInput } from 'react-native-paper';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { AutocompleteDropdown } from 'react-native-autocomplete-dropdown';
 
 function toIsoString(date) {
   var tzo = -date.getTimezoneOffset(),
@@ -62,13 +63,6 @@ const roles = [
   { name: 'Role 2', value: 2 },
   { name: 'Role 3', value: 3 },
   { name: 'Role 4', value: 4 },
-];
-
-const persons = [
-  { name: 'Person 1', value: 1 },
-  { name: 'Person 2', value: 2 },
-  { name: 'Person 3', value: 3 },
-  { name: 'Person 4', value: 4 },
 ];
 
 const EditableDateItem = function (props) {
@@ -138,54 +132,53 @@ const EditableDateItem = function (props) {
 const AssigmentsFormScreen = () => {
   const [entity, setEntity] = useState('');
   const [role, setRole] = useState('');
-  const [person, setPerson] = useState('');
+  const [person, setPerson] = useState(null);
   const [publicNotes, setPublicNotes] = useState('');
   const [startDate, setStartDate] = useState(todayString);
   const [endDate, setEndDate] = useState(todayString);
   const [isCreate, setIsCreate] = useState(true);
   return (
-    <ScrollView>
-      <View
-        style={{
-          width: '100%',
-          height: '100%',
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#F2F3FF',
-        }}
-      >
-        {isCreate ? (
-          <Text
-            style={{
-              fontFamily: 'work-sans-semibold',
-              color: Colors.primaryColor,
-              fontSize: 20,
-              textAlign: 'left',
-              letterSpacing: 2.5,
-              //textTransform: 'uppercase',
-              //margin: 10,
-              padding: 20,
-            }}
-          >
-            Formulario de Asignaciones
-          </Text>
-        ) : (
-          <Text
-            style={{
-              fontFamily: 'work-sans-semibold',
-              color: Colors.primaryColor,
-              fontSize: 20,
-              textAlign: 'left',
-              letterSpacing: 2.5,
-              //textTransform: 'uppercase',
-              //margin: 10,
-              padding: 20,
-            }}
-          >
-            Editar {i18n.t('GENERAL.ASSIGNMENTS')}
-          </Text>
-        )}
-        {
+    <View
+      style={{
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F2F3FF',
+      }}
+    >
+      {isCreate ? (
+        <Text
+          style={{
+            fontFamily: 'work-sans-semibold',
+            color: Colors.primaryColor,
+            fontSize: 20,
+            textAlign: 'left',
+            letterSpacing: 2.5,
+            //textTransform: 'uppercase',
+            //margin: 10,
+            padding: 20,
+          }}
+        >
+          Formulario de Asignaciones
+        </Text>
+      ) : (
+        <Text
+          style={{
+            fontFamily: 'work-sans-semibold',
+            color: Colors.primaryColor,
+            fontSize: 20,
+            textAlign: 'left',
+            letterSpacing: 2.5,
+            //textTransform: 'uppercase',
+            //margin: 10,
+            padding: 20,
+          }}
+        >
+          Editar {i18n.t('GENERAL.ASSIGNMENTS')}
+        </Text>
+      )}
+      {/*
           <View
             style={{
               width: '90%',
@@ -214,44 +207,67 @@ const AssigmentsFormScreen = () => {
               valueChange={(value) => setEntity(value)}
             />
           </View>
-        }
-        {
-          <View
+            */}
+      {
+        <View
+          style={{
+            width: '90%',
+          }}
+        >
+          <Text
             style={{
-              width: '90%',
+              color: Colors.onSurfaceColorSecondary,
+              //color: Colors.onSurfaceColorPrimary,
+              fontWeight: 'bold',
+              //textAlign: 'center',
             }}
+            required
           >
-            <Text
-              style={{
-                color: Colors.onSurfaceColorSecondary,
-                //color: Colors.onSurfaceColorPrimary,
-                fontWeight: 'bold',
-                //textAlign: 'center',
-              }}
-              required
-            >
-              Lista de {i18n.t('GENERAL.ASSIGNMENTS')}
-            </Text>
-            <Select
-              style={{
-                backgroundColor: Colors.surfaceColorSecondary,
-                height: 50,
-                marginVertical: 10,
-                borderRadius: 5,
-              }}
-              elements={roles}
-              value={role}
-              valueChange={(value) => setRole(value)}
-            />
-          </View>
-        }
-        {
-          <View
+            Lista de {i18n.t('GENERAL.ASSIGNMENTS')}
+          </Text>
+          <Select
             style={{
-              width: '90%',
+              backgroundColor: Colors.surfaceColorSecondary,
+              height: 50,
+              marginVertical: 10,
+              borderRadius: 5,
             }}
+            elements={roles}
+            value={role}
+            valueChange={(value) => setRole(value)}
+          />
+        </View>
+      }
+      {
+        <View
+          style={{
+            width: '90%',
+          }}
+        >
+          <Text
+            style={{
+              color: Colors.onSurfaceColorSecondary,
+              //color: Colors.onSurfaceColorPrimary,
+              fontWeight: 'bold',
+              //textAlign: 'center',
+            }}
+            required
           >
-            <Text
+            Lista de Personas
+          </Text>
+          <AutocompleteDropdown
+            containerStyle={{ marginVertical: 10 }}
+            clearOnFocus={false}
+            closeOnSubmit={false}
+            initialValue={{ id: '2' }}
+            onSelectItem={setPerson}
+            dataSet={[
+              { id: '1', title: 'Person1' },
+              { id: '2', title: 'Person2' },
+              { id: '3', title: 'Person3' },
+            ]}
+          />
+          {/*<Text
               style={{
                 color: Colors.onSurfaceColorSecondary,
                 //color: Colors.onSurfaceColorPrimary,
@@ -272,98 +288,96 @@ const AssigmentsFormScreen = () => {
               elements={persons}
               value={person}
               valueChange={(value) => setPerson(value)}
-            />
-          </View>
-        }
-
-        <View
+            />*/}
+        </View>
+      }
+      <View
+        style={{
+          width: '90%',
+        }}
+      >
+        <Text
           style={{
-            width: '90%',
+            //color: Colors.onSurfaceColorPrimary,
+            color: Colors.onSurfaceColorSecondary,
+            fontWeight: 'bold',
+            //textAlign: 'center',
           }}
         >
-          <Text
-            style={{
-              //color: Colors.onSurfaceColorPrimary,
-              color: Colors.onSurfaceColorSecondary,
-              fontWeight: 'bold',
-              //textAlign: 'center',
-            }}
-          >
-            {i18n.t('FATHER_DETAIL.START_DATE')}
-          </Text>
-        </View>
-        <EditableDateItem
-          date={startDate}
-          onDateChange={(value) => {
-            setStartDate(value);
-          }}
-          disabled={true}
-        />
-        <View
-          style={{
-            width: '90%',
-          }}
-        >
-          <Text
-            style={{
-              //color: Colors.onSurfaceColorPrimary,
-              color: Colors.onSurfaceColorSecondary,
-              fontWeight: 'bold',
-              //textAlign: 'center',
-            }}
-          >
-            {i18n.t('FATHER_DETAIL.END_DATE')}
-          </Text>
-        </View>
-        <EditableDateItem
-          date={endDate}
-          onDateChange={(value) => {
-            setEndDate(value);
-          }}
-          disabled={true}
-        />
-        <TextInput
-          style={{
-            width: '90%',
-            height: 50,
-            marginVertical: 10,
-            borderRadius: 5,
-            backgroundColor: '#FFFFFF',
-          }}
-          theme={{ colors: { primary: '#0104AC', underlineColor: 'transparent' } }}
-          label={i18n.t('LIVING_SITUATION.PUBLIC_NOTES')}
-          required
-          autoCapitalize="none"
-          placeholderTextColor={Colors.onSurfaceColorSecondary}
-          value={publicNotes}
-          onChange={(value) => setPublicNotes(value)}
-        />
-        <Button>
-          <View
-            style={{
-              width: '90%',
-              marginVertical: 10,
-              borderRadius: 8,
-              backgroundColor: 'gray',
-              backgroundColor: Colors.primaryColor,
-              paddingHorizontal: 5,
-              paddingVertical: 10,
-            }}
-          >
-            <Text
-              style={{
-                color: 'white',
-                textAlign: 'center',
-                textTransform: 'uppercase',
-                fontFamily: 'work-sans-bold',
-              }}
-            >
-              {i18n.t('FATHER_EDIT.SAVE')}
-            </Text>
-          </View>
-        </Button>
+          {i18n.t('FATHER_DETAIL.START_DATE')}
+        </Text>
       </View>
-    </ScrollView>
+      <EditableDateItem
+        date={startDate}
+        onDateChange={(value) => {
+          setStartDate(value);
+        }}
+        disabled={true}
+      />
+      <View
+        style={{
+          width: '90%',
+        }}
+      >
+        <Text
+          style={{
+            //color: Colors.onSurfaceColorPrimary,
+            color: Colors.onSurfaceColorSecondary,
+            fontWeight: 'bold',
+            //textAlign: 'center',
+          }}
+        >
+          {i18n.t('FATHER_DETAIL.END_DATE')}
+        </Text>
+      </View>
+      <EditableDateItem
+        date={endDate}
+        onDateChange={(value) => {
+          setEndDate(value);
+        }}
+        disabled={true}
+      />
+      <TextInput
+        style={{
+          width: '90%',
+          height: 50,
+          marginVertical: 10,
+          borderRadius: 5,
+          backgroundColor: '#FFFFFF',
+        }}
+        theme={{ colors: { primary: '#0104AC', underlineColor: 'transparent' } }}
+        label={i18n.t('LIVING_SITUATION.PUBLIC_NOTES')}
+        required
+        autoCapitalize="none"
+        placeholderTextColor={Colors.onSurfaceColorSecondary}
+        value={publicNotes}
+        onChange={(value) => setPublicNotes(value)}
+      />
+      <Button>
+        <View
+          style={{
+            width: '90%',
+            marginVertical: 10,
+            borderRadius: 8,
+            backgroundColor: 'gray',
+            backgroundColor: Colors.primaryColor,
+            paddingHorizontal: 5,
+            paddingVertical: 10,
+          }}
+        >
+          <Text
+            style={{
+              color: 'white',
+              textAlign: 'center',
+              textTransform: 'uppercase',
+              fontFamily: 'work-sans-bold',
+            }}
+          >
+            {i18n.t('FATHER_EDIT.SAVE')}
+          </Text>
+        </View>
+      </Button>
+    </View>
   );
 };
 AssigmentsFormScreen.navigationOptions = (navigationData) => ({
