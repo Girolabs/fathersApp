957963b20ad811de7e53411bf24679c295986697
Andres Goyburu <andres@girolabs.com>
2023-01-20 14:55:49 -0300

    Remove unused package

--- src/i18n/index.js
+++
@@ -1,8 +0,0 @@
-/* import i18n from 'i18next';
-import { reacI18nextModule } from 'react-i18next';
-import locale from 'react-native-locale-detector';
-import { AsyncStorage } from 'react-native';
-
-import en from './en.json';
-import es from './es.json';
- */
