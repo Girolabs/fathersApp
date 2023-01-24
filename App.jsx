94726a992175bcf7e6a011dd677ccecc7781dc2a
AlexGC <alex1995gc@gmail.com>
2023-01-24 13:02:17 -0300

    changes to build

--- App.jsx
+++ App.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-// import * as Sentry from 'sentry-expo';
-// import {
-//   useFonts,
-//   WorkSans_400Regular,
-//   WorkSans_500Medium,
-//   WorkSans_600SemiBold,
-//   WorkSans_700Bold,
-// } from '@expo-google-fonts/inter';
+
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import PatresNavigator from './src/navigator/PatresNavigator';
 import I18nProvider from './src/context/I18nProvider';
 import AuthProvider from './src/context/AuthProvider';
 import BulletinCheckProvider from './src/context/BulletinCheckProvider';
-
-// Sentry.init({
-//   dsn: 'https://9f3fff92f0324708b8ef98da9c96b5e7@o464423.ingest.sentry.io/5473069',
-//   enableInExpoDevelopment: true,
-//   debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
-// });
 
 export default function App() {
   const [fontsLoaded] = useFonts({
