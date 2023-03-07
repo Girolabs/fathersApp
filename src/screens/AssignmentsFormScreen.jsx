2228b29276032b3e8200540e52db61c2e22376f5
alexis gali <alex1995gc@gmail.com>
2023-03-07 11:43:30 -0300

    new version: issues 268, 282, 283, 285

--- src/screens/AssignmentsFormScreen.jsx
+++ src/screens/AssignmentsFormScreen.jsx
@@ -113,16 +113,16 @@ const EditableDateItem = function (props) {
             onChange={(event, val) => {
               const formatDate = (val) => {
                 let fecha = new Date(val);
-                const dia = fecha.getUTCDate().toString().padStart(2, '0');
-                const mes = (fecha.getUTCMonth() + 1).toString().padStart(2, '0');
-                const year = fecha.getUTCFullYear().toString();
+                const dia = fecha.getDate().toString().padStart(2, '0');
+                const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+                const year = fecha.getFullYear().toString();
                 return `${year}-${mes}-${dia}`;
               };
               if (Platform.OS === 'android') {
                 setShow(false);
               }
               setShowOk(true);
-              const pickedDate = Platform.OS === 'android' ? formatDate(val) : formatDate(val);
+              const pickedDate = Platform.OS === 'android' ? formatDate(val) : val.toISOString().split("T")[0];
               if (event.type === 'set') {
                 props.onDateChange(pickedDate);
               }
